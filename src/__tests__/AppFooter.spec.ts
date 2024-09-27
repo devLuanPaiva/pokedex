29ddde88501378c5ad07ea5fb085c64f65203b07
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-27 13:33:14 -0300

    feat: testing render footer with sections

---
+++ src/__tests__/AppFooter.spec.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import AppFooter from '@/components/shared/AppFooter.vue'
+
+describe('AppFooter.vue', () => {
+  it('should render footer with sections', () => {
+    const wrapper = mount(AppFooter, {
+      global: {
+        stubs: ['router-link'], 
+      }
+    })
+
+    expect(wrapper.html()).toContain('Sobre o Pokebox')
+
+    expect(wrapper.html()).toContain('Links Úteis')
+
+    expect(wrapper.html()).toContain('Siga-nos')
+  })
+})
