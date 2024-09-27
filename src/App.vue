e1adb8e124253d5462f2dc60e03e3518dd65fe2f
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-27 13:09:04 -0300

    feat: add navbar and footer in the App main

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
-  import {ref} from 'vue'
-  let num = ref(0)
+import Nav from './components/shared/AppNav.vue'
+import Footer from './components/shared/AppFooter.vue'
 </script>
 
 <template>
-  <div>
-    <button @click="num">
-      {{num }}
-    </button>
+  <div class="app-container">
+    <Nav />
+    <main class="main">
+      <router-view></router-view>
+    </main>
+    <Footer/>
   </div>
 </template>
 
+<style scoped>
+html, body {
+    height: 100%; 
+    margin: 0;
+    padding: 0;
+}
+
+.app-container {
+    display: flex;
+    flex-direction: column; 
+    min-height: 100vh; 
+}
+
+.main {
+    flex: 1; 
+}
+</style>
