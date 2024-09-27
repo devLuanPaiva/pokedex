c892eec5b7497690a41c8026c9d4d453f4f5dada
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-27 15:12:16 -0300

    feat: create AppCard component for show the pokemon

---
+++ src/components/pokemon/AppCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <article class="card col-sm-12 col-md-6" style="width: 18rem">
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR--cGc4QPfdI_m_QS605oDdH-zARKYQgvB9g&s"
+      class="card-img-top"
+      alt="pokemon"
+    />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk of the card's
+        content.
+      </p>
+    </div>
+  </article>
+</template>
+<script setup lang="ts"></script>
