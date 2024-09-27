ae879f625c5b182f706894dc443c2ef55918c8e7
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-27 15:13:56 -0300

    feat: fetch pokemon in the PokeAPI and list in component

---
+++ src/components/pokemon/AppListPokemons.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted, reactive } from 'vue'
+
+let pokemons = reactive({list:[]})
+
+onMounted(() => {
+    console.log('mo')
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    .then((response) => response.json())
+    .then(response => {
+        pokemons.list = response.results
+    })
+})
+</script>
+
+<template>
+    <div>
+      <h2>List of Pokémons (Limited to 151)</h2>
+      <article class="card" style="width: 18rem;">
+        <ul>
+          <li v-for="pokemon in pokemons.list" :key="pokemon.name">
+            {{ pokemon.name }}
+          </li>
+        </ul>
+      </article>
+    </div>
+  </template>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-
+<script setup>
+import AppCard from '../components/pokemon/AppCard.vue';
+import AppListPokemons from '../components/pokemon/AppListPokemons.vue'
 </script>
 
 <template>
-  <main>
-   home
-  </main>
+  <section class="container pt-4">
+    <div class="d-flex justify-content-between flex-sm-column flex-md-row gap-3">
+      <AppCard />
+      <AppListPokemons/>
+    </div>
+  </section>
 </template>
