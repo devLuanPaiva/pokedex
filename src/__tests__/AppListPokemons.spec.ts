50a692f4c889b7074da9b7162a25f76ecc71935f
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-29 15:54:30 -0300

    refactor: adjusting the fetch Mock

--- src/__tests__/AppListPokemons.spec.ts
+++ src/__tests__/AppListPokemons.spec.ts
@@ -5,15 +5,40 @@ import AppPokemonItem from '@/components/pokemon/AppPokemonItem.vue'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
-global.fetch = vi.fn(() =>
-  Promise.resolve({
-    ok: true,
-    json: () =>
-      Promise.resolve({
-        results: [{ name: 'bulbasaur' }, { name: 'ivysaur' }, { name: 'venusaur' }]
-      })
-  } as Response)
-)
+global.fetch = vi.fn((input: URL | RequestInfo) => {
+  let url: string
+
+  if (typeof input === 'string') {
+    url = input
+  } else if (input instanceof URL) {
+    url = input.toString()
+  } else if (input instanceof Request) {
+    url = input.url
+  } else {
+    return Promise.reject(new Error('Input inválido'))
+  }
+
+  if (url.includes('pokemon?limit=300&offset=0')) {
+    return Promise.resolve({
+      ok: true,
+      json: () =>
+        Promise.resolve({
+          results: [{ name: 'bulbasaur' }, { name: 'ivysaur' }, { name: 'venusaur' }]
+        })
+    } as Response)
+  } else if (url.includes('pokemon/')) {
+    return Promise.resolve({
+      ok: true,
+      json: () =>
+        Promise.resolve({
+          name: url.split('/').pop(),
+          types: [{ type: { name: 'grass' } }]
+        })
+    } as Response)
+  }
+
+  return Promise.reject(new Error('URL não reconhecida'))
+})
 
 describe('AppListPokemons.vue', () => {
   let wrapper: any
@@ -31,13 +56,13 @@ describe('AppListPokemons.vue', () => {
   })
 
   it('fetches Pokémon data on mount', async () => {
-    await wrapper.vm.$nextTick()  
-    await new Promise(resolve => setTimeout(resolve, 100)); 
+    await wrapper.vm.$nextTick()
+    await new Promise((resolve) => setTimeout(resolve, 100))
 
-    expect(wrapper.vm.pokemons.list).toHaveLength(3) 
+    expect(wrapper.vm.pokemons.list).toHaveLength(3)
 
     const pokemonItems = wrapper.findAllComponents(AppPokemonItem)
-    expect(pokemonItems).toHaveLength(3) 
+    expect(pokemonItems).toHaveLength(3)
     expect(pokemonItems[0].props().name).toBe('bulbasaur')
     expect(pokemonItems[1].props().name).toBe('ivysaur')
     expect(pokemonItems[2].props().name).toBe('venusaur')
