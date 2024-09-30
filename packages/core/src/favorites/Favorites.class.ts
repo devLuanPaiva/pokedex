30588b469e4b2176677484bc3e3ed480b626e991
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-30 15:05:40 -0300

    feat: add class Favorites

---
+++ packages/core/src/favorites/Favorites.class.ts
@@ -0,0 +1,27 @@
+import IFavorites from "./IFavorites.interface";
+import RepositoryFavorites from "./RepositoryFavorites.interface";
+
+export default class Favorites {
+  constructor(private readonly repo: RepositoryFavorites) {}
+
+  async addFavorite(favorite: IFavorites): Promise<void> {
+    const existingFavorites = await this.repo.findByPokemonId(
+      favorite.user.id,
+      favorite.pokemonName
+    );
+    if (existingFavorites) {
+      throw new Error("Já adicionado aos favoritos!");
+    }
+    await this.repo.save(favorite);
+  }
+  async removeFavorite(favorite: IFavorites): Promise<void> {
+    const existingFavorites = await this.repo.findByPokemonId(
+      favorite.user.id,
+      favorite.pokemonName
+    );
+    if (!existingFavorites) {
+      throw new Error("Pokémon não encontrado nos favoritos!");
+    }
+    await this.repo.delete(favorite);
+  }
+}
---
+++ packages/core/src/favorites/index.ts
@@ -0,0 +1,6 @@
+import Favorites from "./Favorites.class";
+import IFavorites from "./IFavorites.interface";
+import RepositoryFavorites from "./RepositoryFavorites.interface";
+
+export { Favorites };
+export type { IFavorites, RepositoryFavorites };
