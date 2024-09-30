559b7e3705b2efdb10c7a7566db1e459365505c0
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-30 16:23:11 -0300

    feat: implements controller api of favorites in backend

---
+++ apps/backend/src/controllers/Favorites.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { Favorites, IFavorites } from "@poke/core";
+import { FavoritesRepository } from "../repository/FavoritesRepository";
+
+const favoritesRepo = new FavoritesRepository();
+const favoritesService = new Favorites(favoritesRepo);
+
+export class FavoritesController {
+  async add(req: Request, res: Response) {
+    const favorite: IFavorites = req.body;
+
+    try {
+      await favoritesService.addFavorite(favorite);
+      return res
+        .status(201)
+        .json({ message: "Pokémon adicionado aos favoritos!" });
+    } catch (error: any) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
+
+  async remove(req: Request, res: Response) {
+    const favorite: IFavorites = req.body;
+
+    try {
+      await favoritesService.removeFavorite(favorite);
+      return res
+        .status(204)
+        .send()
+        .json({ message: "Pokémon removido dos favoritos!" });
+    } catch (error: any) {
+      return res.status(404).json({ message: error.message });
+    }
+  }
+}
