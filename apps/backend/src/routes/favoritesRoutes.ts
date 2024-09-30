70b9c110dfcd2cc7768a20b3260e75e9feab6a8f
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-30 16:55:04 -0300

    feat: add new routes for user

--- apps/backend/src/routes/favoritesRoutes.ts
+++ apps/backend/src/routes/favoritesRoutes.ts
@@ -1,15 +1,25 @@
 import { Router } from "express";
 import { FavoritesController } from "../controllers/Favorites.controller";
+import { UserController } from "../controllers/User.controller";
 
 const router = Router();
 const favoritesController = new FavoritesController();
+const userController = new UserController();
 
 router.post("/favorites/add", async (req, res) => {
   await favoritesController.add(req, res);
 });
 
 router.delete("/favorites/remove", async (req, res) => {
-  favoritesController.remove(req, res);
+  await favoritesController.remove(req, res);
+});
+
+router.post("user/register", async (req, res) => {
+  await userController.register(req, res);
+});
+
+router.post("user/login", async (req, res) => {
+  await userController.login(req, res);
 });
 
 export default router;
