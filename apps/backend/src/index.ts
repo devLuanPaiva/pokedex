ca8992d879d6acd28729b84aad3c2d6d65b15d2b
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-01 09:37:20 -0300

    refactor: add cors in aplication

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";  
 import favoritesRoutes from "./routes/favoritesRoutes";
 
 const app = express();
+
+app.use(cors()); 
+
 app.use(express.json());
 
 app.use("/api", favoritesRoutes);
