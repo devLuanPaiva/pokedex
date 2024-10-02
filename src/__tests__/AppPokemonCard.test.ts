57b5e7ffde33b87e8cbfe5093bf2dc71777d0884
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-02 15:53:59 -0300

    feat: testing toggles favorite when heart icon is clicked

---
+++ src/__tests__/AppPokemonCard.test.ts
@@ -0,0 +1,30 @@
+import AppCard from '@/components/pokemon/AppCard.vue'
+import { mount } from '@vue/test-utils'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { createPinia, setActivePinia } from 'pinia'
+import { usePokemonStore } from '@/stores/usePokemonStore'
+
+describe('AppCard', () => {
+  let pokemonStore: ReturnType<typeof usePokemonStore>
+
+  beforeEach(() => {
+    const pinia = createPinia()
+    setActivePinia(pinia)
+
+    pokemonStore = usePokemonStore()
+    pokemonStore.selectedPokemon = 'pikachu' 
+  })
+
+
+  it('toggles favorite when heart icon is clicked', async () => {
+    const wrapper = mount(AppCard)
+
+    await wrapper.find('.heart-icon').trigger('click')
+
+    expect(pokemonStore.favorites).toContain('pikachu')
+
+    await wrapper.find('.heart-icon').trigger('click')
+
+    expect(pokemonStore.favorites).not.toContain('pikachu')
+  })
+})
