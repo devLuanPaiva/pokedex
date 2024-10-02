cb7430a358fd6582684a1234903ae261232a5840
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-02 14:17:49 -0300

    feature: implements pinia for global state of favorites pokemons

--- src/stores/usePokemonStore.ts
+++ src/stores/usePokemonStore.ts
@@ -3,13 +3,38 @@ import { ref } from 'vue'
 
 export const usePokemonStore = defineStore('pokemon', () => {
   const selectedPokemon = ref<string | null>(null)
+  const favorites = ref<string[]>([])
 
-  function selectPokemon(name: string) {
+  const loadFavoritesFromLocalStorage = () => {
+    const storedFavorites = localStorage.getItem('pokemonsFavorites')
+    if (storedFavorites) {
+      favorites.value = JSON.parse(storedFavorites)
+    }
+  }
+
+  const toggleFavorites = (pokemon: string) => {
+    const index = favorites.value.indexOf(pokemon)
+    if (index === -1) {
+      favorites.value.push(pokemon)
+    } else {
+      favorites.value.splice(index, 1)
+    }
+    localStorage.setItem('pokemonsFavorites', JSON.stringify(favorites.value))
+  }
+
+  const isFavorite = (pokemon: string) => favorites.value.includes(pokemon)
+
+  const selectPokemon = (name: string) => {
     selectedPokemon.value = name
   }
 
+  loadFavoritesFromLocalStorage()
+
   return {
     selectedPokemon,
-    selectPokemon
+    selectPokemon,
+    favorites,
+    isFavorite,
+    toggleFavorites
   }
 })
